f1cc8c3e5787280748b8b944581c9ba143baa5a9
Nathaniel Lee <nathaniel.k.lee@gmail.com>
2025-02-15 18:26:31 -0500

    Added fluent provider for theming.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { FluentProvider, webLightTheme } from '@fluentui/react-components';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <FluentProvider theme={webLightTheme}>
+      <App />
+    </FluentProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
